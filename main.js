9968b9dcf0540d0b722e674efbcd39da12af9479
Артур <arthur.betsa@gmail.com>
2020-05-09 05:57:55 +0300

    fixe functions

--- main.js
+++ main.js
@@ -1,38 +1,50 @@
 "use strict"
 ;
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11];
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 let letters = ["a", "b", "c", "d", "e", "f"];
+const forbidden_paswords = ["1234", "12345", "654321", "qwerty", "password", "qazwsx"];
+
 
 function findWile(arr, val) {
+    let result = false;
     let min = 0;
     let max = arr.length;
-    let middle = Math.floor((min + max) / 2);
+    let middle;
     let stepCount = 0;
 
-    while (min < max) {
+    while (min <= max && !result) {
+        middle = Math.floor((min + max) / 2);
+        console.log("min: ", min);
+        console.log("max: ", max);
+
         stepCount++;
         if (val === arr[middle]) {
-            return console.log(`(W)Count of steps: ${stepCount}; Index of Value: ${middle};`);
+            result++;
+            return console.log(`(W)Count of steps: ${stepCount}; Value: ${val} founded in index: ${middle};`);
         } else {
-            if (val <= arr[middle]) {  //кто блять знал, что нужно еще и отнимать и добавлять к среднему значению, иначе при значении не входящем в масив будет бесконечный цикл
+            if (val < arr[middle]) {  //кто блять знал, что нужно еще и отнимать и добавлять к среднему значению, иначе при значении не входящем в масив будет бесконечный цикл
                 max = middle - 1;
             } else {
                 min = middle + 1;
             }
 
         }
-        middle = Math.floor((min + max) / 2);
+
     }
     return console.log("(W)Out of range ");
 }
 
+//
 // console.group(`function with "while"`);
 //
 // findWile(numbers, 0);
+// findWile(numbers, -1);
 // findWile(numbers, 3);
 // findWile(numbers, 5);
 // findWile(numbers, 10);
-// findWile(numbers, "a");
+// findWile(letters, "a");
+// findWile(letters, "c");
+// findWile(letters, "d");
 // findWile(numbers, 144);
 // console.groupEnd();
 
@@ -47,13 +59,12 @@ function findFor(arr, val) {
         stepCount++;
         if (val === arr[middle]) {
             return console.log(`(F)Count of steps: ${stepCount}; Value: ${val} founded in index of ${middle}`);
+        } else if (val <= arr[middle]) {
+            max = middle - 1;
         } else {
-            if (val <= arr[middle]) {
-                max = middle;
-            } else {
-                min = middle;
-            }
+            min = middle + 1;
         }
+
         middle = Math.floor((min + max) / 2);
     }
 
@@ -63,50 +74,49 @@ function findFor(arr, val) {
 
 // console.group(`function with "for"`);
 // findFor(numbers, 0);
-// findFor(numbers, 3);
+// // findFor(numbers, 3);
 // findFor(numbers, 5);
-// findFor(numbers, 10);
+// // findFor(numbers, 10);
 // findFor(numbers, "a");
 // findFor(numbers, 11);
 // console.groupEnd();
+// findFor(forbidden_paswords, "1234");
 
-
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+let letters = ["a", "b", "c", "d", "e", "f"];
 
 function findByRecursion(arr, val) {
     let min = 0;
     let max = arr.length;
-    let midle = Math.floor((min + max) / 2);
+    let middle;
     let stepCount = 0;
     let result = false;
 
     function find(min, max) {
         stepCount++;
-        if (!((max-1 === min || min+1 === max) && result === false  )|| max-1 ===0 ) {
-            if (val === arr[midle]) {
-                return result = arr[midle];
-            } else {
-                if (val <= arr[midle]) {
-                    max = midle;
-                } else {
-                    min = midle;
-                }
-                midle = Math.floor((min + max) / 2);
-                find(min, max);
-            }
-        } else {
-            console.log("(F)Out of range");
-            return false;
+        middle = Math.floor((min + max) / 2);
+
+        if (min > max || max < min) {
+            return result;
         }
+        if (val === arr[middle]) {
+            return result++;
+        }
+        if (val <= arr[middle]) {
+            max = middle - 1;
+        } else {
+            min = middle + 1;
+        }
+        find(min, max);
     }
 
     find(min, max);
 
-    !result
-        ? console.log(`val: ${val} NOT founded`)
-        : console.log(`val: ${val} founded in index of: ${midle}`)
+    result
+        ? console.log(`val: ${val} founded in index of: ${middle}, step: ${stepCount}.`)
+        : console.log(`val: ${val} NOT founded, step: ${stepCount}.`)
 
 }
 
-findByRecursion(letters, "a");
-findByRecursion(numbers, 0);
-
+// findByRecursion(letters, "a");
+// findByRecursion(numbers, 112);
